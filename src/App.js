ab291e1889ccbac1b3ded15665b5e0fd24faae8b
Pinki Paul <pinkipaul371@gmail.com>
2024-01-31 10:51:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const handleClickFunction = (e) =>{
+     const body = document.querySelector("body");
+     const heading1 = document.querySelector("h1");
+     heading1.style.color = getRandomColor();
+     body.style.background = getRandomColor();
+  }
+
+  const getRandomColor = () => {
+    let letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let hashSign = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hashSign += letters[Math.floor(Math.random() * 16)];
+    }
+    return hashSign;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className='heading'>Background Color Changer App</h1>
+        <button className='btn' onClick={handleClickFunction}>Click to Change</button>
+      </div>
+    </>
+
   );
 }
 
